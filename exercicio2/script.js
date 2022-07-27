a89620e4b62956785ea192422d1c3389675c7af9
Joanna Souza <joannasouza40@gmail.com>
2022-07-26 21:29:00 -0300

    Exercícios corrigidos na aula

--- exercicio2/script.js
+++ exercicio2/script.js
@@ -1,1 +1,11 @@
-const minhaString = "Eu não sou supersticioso, mas sou um pouco ________.      ";
+//Exercicio 2
+
+const minhaString = "Eu não sou supersticioso, mas sou um pouco ________.      " ;
+console.log(minhaString.length)
+
+const novaStringSemEspaco = minhaString.trim()
+console.log(novaStringSemEspaco.length)
+
+const substituicao = novaStringSemEspaco.replace( "________.", "sticioso");
+
+console.log(substituicao)
